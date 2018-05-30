9a991118d27b1c1dbe4aea1c5a1f14b67551586d
Martin Volf <mvolf@cisco.com>
2018-05-30 15:24:24 +0200

    transition, setup, state tests working
    Signed-off-by: Martin Volf <mvolf@cisco.com>

--- test/unit/actions.py
+++ test/unit/actions.py
@@ -1,6 +1,12 @@
-from .mocklib import mock, system_mock, ncs_mock, open_mock, xtest_patch
-from drned_xmnr import action
+from __future__ import print_function
 
+from . import mocklib
+from .mocklib import mock, xtest_patch
+from drned_xmnr import action
+from drned_xmnr.op import config_op, base_op
+import os
+import functools
+import itertools
 
 device_data = '''
     <config xmlns="http://tail-f.com/ns/config/1.0">
@@ -20,24 +26,277 @@ device_data = '''
 '''
 
 
+test_state_data = '''
+test state data
+'''
+
+
 class TestBase(object):
+    real_open = open
+    log_levels = ('error', 'warning', 'info', 'debug')
+    test_run_dir = os.path.join(mocklib.XMNR_DIRECTORY, mocklib.DEVICE_NAME, 'test')
+    states = ('state1', 'state2', 'otherstate1')
+
+    def check_output(self, output, success=None):
+        assert output.error is None
+        assert output.failure is None
+        if success is not None:
+            assert output.success == success
+
+    def unit_log_file(self):
+        if not hasattr(self, 'log_file'):
+            self.log_file = self.real_open('/tmp/unit.log', 'a')
+        return self.log_file
+
+    def setup_log(self, handler):
+        handler.log = mock.Mock()
+        for level in self.log_levels:
+            prefix = '<{}>'.format(level.upper())
+            setattr(handler.log, level, functools.partial(print, prefix, file=self.unit_log_file()))
+
+    def invoke_action(self, action_name, **action_params):
+        params = mock.Mock(spec=action_params.keys(), **action_params)
+        ah = action.ActionHandler()
+        self.setup_log(ah)
+        kp = [[mocklib.DEVICE_NAME], None, None]
+        output = mock.Mock(error=None, failure=None, success=None)
+        ah.cb_action(mock.Mock(), action_name, kp, params, output)
+        return output
+
+    def setup_states_data(self, system, state_path=None):
+        spath = state_path
+        if spath is None:
+            spath = os.path.join(self.test_run_dir, 'states')
+        for state in self.states:
+            stname = state + base_op.XmnrBase.statefile_extension
+            system.ff_patcher.fs.create_file(os.path.join(spath, stname),
+                                             contents='{} test data'.format(state))
+            if state_path is None:
+                system.ff_patcher.fs.create_file(os.path.join(spath, stname + '.load'),
+                                                 contents=config_op.state_metadata)
+
+
+class TestStartup(TestBase):
     def test_registry(self):
         xmnr = action.Xmnr()
         xmnr.setup()
         xmnr.register_action.assert_called_once_with('drned-xmnr', action.ActionHandler)
-        xmnr.register_service.assert_called_once_with('coverage-data', action.XmnrDataHandler)
+        xmnr.register_service.assert_has_calls([mock.call('coverage-data', action.XmnrDataHandler),
+                                                mock.call('xmnr-states', action.XmnrDataHandler)])
 
-    @xtest_patch(system_mock, ncs_mock, open_mock('setup_op'))
+
+class TestSetup(TestBase):
+    def setup_fs_data(self, system):
+        system.ff_patcher.fs.create_dir(mocklib.XMNR_DIRECTORY)
+        system.ff_patcher.fs.add_real_file('/dev/null')
+        system.ff_patcher.fs.create_file(os.path.join(mocklib.XMNR_INSTALL, 'drned', 'skeleton'),
+                                         contents='drned skeleton')
+
+    def setup_ncs_data(self, ncs):
+        device = ncs.data['device']
+        device.device_type = mock.Mock(netconf='netconf', ne_type='netconf')
+
+    @xtest_patch
     def test_setup(self, xpatch):
-        ah = action.ActionHandler()
-        ah.log = mock.Mock()
-        import functools
-        ah.log.debug = functools.partial(print, file=open('/tmp/unit.log', 'w'))
-        output = mock.Mock(error=None, failure=None)
-        kp = [['mock-device'], None, None]
-        params = mock.Mock(overwrite=True)
+        self.setup_fs_data(xpatch.system)
+        self.setup_ncs_data(xpatch.ncs)
         xpatch.system.socket_data(device_data)
-        ah.cb_action(mock.Mock(), 'setup-xmnr', kp, params, output)
-        print('tree', xpatch.system.tree)
-        assert output.error is None
-        assert output.failure is None
+        output = self.invoke_action('setup-xmnr', overwrite=True)
+        self.check_output(output)
+        with open(os.path.join(self.test_run_dir, 'drned/skeleton')) as skel_test:
+            assert skel_test.read() == 'drned skeleton'
+        popen_mock = xpatch.system.patches['subprocess']['Popen']
+        popen_mock.assert_called_once()
+        assert popen_mock.call_args[0] == (['make', 'env.sh'],)
+
+
+class TestStates(TestBase):
+    def state_files(self, states):
+        return sorted(itertools.chain(*((st + '.state.cfg', st + '.state.cfg.load')
+                                        for st in states)))
+
+    def check_states(self, states):
+        assert (sorted(os.listdir(os.path.join(self.test_run_dir, 'states'))) ==
+                self.state_files(states))
+
+    @xtest_patch
+    def test_states_data(self, xpatch):
+        log = mock.Mock()
+        sp = config_op.StatesProvider(log)
+        self.setup_log(sp)
+        self.setup_states_data(xpatch.system)
+        tctx = mock.Mock()
+        obj = sp.get_object(tctx, None, {'device': mocklib.DEVICE_NAME})
+        assert sorted(st['state'] for st in obj['states']) == sorted(self.states)
+
+    @xtest_patch
+    def test_list_states(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('list-states')
+        self.check_output(output)
+        part = 'Saved device states: '
+        assert output.success.startswith(part)
+        rest = output.success[len(part):]
+        rstates = eval(rest)
+        assert sorted(rstates) == sorted(self.states)
+
+    @xtest_patch
+    def test_record_state(self, xpatch):
+        xpatch.system.socket_data(test_state_data)
+        output = self.invoke_action('record-state',
+                                    state_name='test_state',
+                                    including_rollbacks=None)
+        self.check_output(output)
+        state_path = 'states/test_state' + base_op.XmnrBase.statefile_extension
+        assert os.path.exists(os.path.join(self.test_run_dir, state_path))
+        with open(os.path.join(self.test_run_dir, state_path + '.load')) as metadata:
+            assert metadata.read() == config_op.state_metadata
+        with open(os.path.join(self.test_run_dir, state_path)) as state_data:
+            assert state_data.read() == test_state_data
+
+    @xtest_patch
+    def test_import_states(self, xpatch):
+        path = '/tmp/data'
+        xpatch.system.ff_patcher.fs.create_dir(path)
+        self.setup_states_data(xpatch.system, state_path=path)
+        output = self.invoke_action('import-state-files',
+                                    file_path_pattern=os.path.join(path, '*1.state.cfg'),
+                                    overwrite=None)
+        self.check_output(output)
+        destdir = os.path.join(self.test_run_dir, 'states')
+        states = sorted(state for state in self.states if state.endswith('1'))
+        self.check_states(states)
+        for state in states:
+            filename = os.path.join(destdir, state + '.state.cfg')
+            with open(filename) as state_file:
+                assert state_file.read() == '{} test data'.format(state)
+            with open(filename + '.load') as metadata:
+                assert metadata.read() == config_op.state_metadata
+
+    @xtest_patch
+    def test_delete_states_pattern(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('delete-state',
+                                    state_name=None,
+                                    state_name_pattern='*1')
+        self.check_output(output)
+        self.check_states(st for st in self.states if not st.endswith('1'))
+
+    @xtest_patch
+    def test_delete_state(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('delete-state',
+                                    state_name_pattern=None,
+                                    state_name='otherstate1')
+        self.check_output(output)
+        states = list(self.states[:])
+        states.remove('otherstate1')
+        self.check_states(states)
+
+    @xtest_patch
+    def test_delete_failure(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('delete-state',
+                                    state_name_pattern=None,
+                                    state_name='no-such-state')
+        assert output.failure is not None
+        self.check_states(self.states)
+
+
+class TestTransitions(TestBase):
+    stop_names = ['seconds', 'minutes', 'hours', 'days', 'percent', 'cases']
+    stop_params = {stop: None for stop in stop_names}
+
+    def check_drned_call(self, call, state=None, rollback=False, fnames=None):
+        if fnames is None:
+            test = 'test_template_single' if rollback else 'test_template_raw'
+            test_args = ['-k {}[../states/{}.state.cfg]'.format(test, state)]
+        else:
+            test_args = ['--fname={}'.format(os.path.join(self.test_run_dir,
+                                                          'states',
+                                                          state + '.state.cfg'))
+                         for state in self.states]
+            test_args += ['--unsorted', '-k', 'test_template_set']
+        args = ['py.test', '-s', '--tb=short', '--device=' + mocklib.DEVICE_NAME, '--unreserved']
+        args += test_args
+        assert call[0] == (args,)
+
+    @xtest_patch
+    def test_transition_to_state(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('transition-to-state',
+                                    state_name='state1',
+                                    rollback=False)
+        self.check_output(output)
+        popen_mock = xpatch.system.patches['subprocess']['Popen']
+        popen_mock.assert_called_once()
+        self.check_drned_call(popen_mock.call_args, 'state1', rollback=False)
+
+    @xtest_patch
+    def test_transition_to_state_rollback(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('transition-to-state',
+                                    state_name='state1',
+                                    rollback=True)
+        self.check_output(output)
+        popen_mock = xpatch.system.patches['subprocess']['Popen']
+        popen_mock.assert_called_once()
+        self.check_drned_call(popen_mock.call_args, 'state1', rollback=True)
+
+    @xtest_patch
+    def test_transition_to_state_failed(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('transition-to-state',
+                                    state_name='no-such-state',
+                                    rollback=True)
+        popen_mock = xpatch.system.patches['subprocess']['Popen']
+        popen_mock.assert_not_called()
+        assert output.failure is not None
+        assert output.failure.startswith('No such state')
+
+    @xtest_patch
+    def test_explore_states(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('explore-transitions',
+                                    states=self.states,
+                                    stop_after=mock.Mock(**self.stop_params))
+        self.check_output(output)
+        popen_mock = xpatch.system.patches['subprocess']['Popen']
+        ls = len(self.states)
+        assert len(popen_mock.call_args_list) == ls*ls
+        calls = iter(popen_mock.call_args_list)
+        for from_state in self.states:
+            self.check_drned_call(next(calls), from_state)
+            for to_state in self.states:
+                if from_state == to_state:
+                    continue
+                self.check_drned_call(next(calls), to_state, rollback=True)
+
+    def popen_fail_state(self, args, *rest, **kwargs):
+        for arg in args:
+            if 'otherstate1' in arg:
+                return mock.Mock(wait=mock.Mock(return_value=-1))
+        return mock.DEFAULT
+
+    @xtest_patch
+    def test_explore_failure(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        popen_mock = xpatch.system.patches['subprocess']['Popen']
+        popen_mock.side_effect = self.popen_fail_state
+        output = self.invoke_action('explore-transitions',
+                                    states=self.states,
+                                    stop_after=mock.Mock(**self.stop_params))
+        assert output.success is None
+        assert 'Failed to initialize state otherstate1' in output.error
+        assert '==> otherstate1' in output.failure
+
+    @xtest_patch
+    def test_walk_states(self, xpatch):
+        self.setup_states_data(xpatch.system)
+        output = self.invoke_action('walk-states',
+                                    states=self.states,
+                                    stop_after=mock.Mock(**self.stop_params))
+        self.check_output(output)
+        popen_mock = xpatch.system.patches['subprocess']['Popen']
+        self.check_drned_call(popen_mock.call_args, fnames=self.states)
+        popen_mock.assert_called_once()
--- test/unit/mocklib.py
+++ test/unit/mocklib.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 from contextlib import contextmanager
 import functools
+from pyfakefs import fake_filesystem_unittest as ffs
 if sys.version_info >= (3, 3):
     import unittest.mock as mock
 else:
@@ -13,7 +14,11 @@ Mock = mock.Mock
 
 
 class MaapiMock(Mock):
-    pass
+    def __enter__(self):
+        return self
+
+    def __exit__(*ignore):
+        pass
 
 
 @contextmanager
@@ -46,16 +51,16 @@ class XtestPatch(object):
             with nest_mgrs([gen() for gen in self.mock_gens]) as mocks:
                 for mock_inst in mocks:
                     setattr(self, mock_inst.name, mock_inst)
-                if hasattr(self, 'system') and hasattr(self, 'open'):
-                    self.system.set_open_mocks(self.open)
                 args = list(args)
                 args.append(self)
                 fn(*args)
         return wrapper
 
 
-def xtest_patch(*mock_gens):
-    return XtestPatch(mock_gens)
+def xtest_patch(*patch_args):
+    if len(patch_args) == 1 and callable(patch_args[0]):
+        return XtestPatch((system_mock, ncs_mock))(patch_args[0])
+    return XtestPatch(patch_args)
 
 
 class XtestMock(object):
@@ -63,21 +68,30 @@ class XtestMock(object):
         self.name = name
 
 
+XMNR_DIRECTORY = 'test_xmnr_dir'
+DRNED_DIRECTORY = 'drned_dir'
+DEVICE_NAME = 'mock-device'
+XMNR_INSTALL = 'xmnr-install'
+
+
 @contextmanager
 def ncs_mock():
     device = Mock(device_type=Mock(ne_type='netconf', netconf='netconf'))
-    rootmock = Mock(devices=Mock(device={'mock-device': device}),
-                    packages=Mock(package={'drned-xmnr': Mock(directory='xmnr-install')}),
-                    drned_xmnr=Mock(xmnr_directory='test_xmnr_dir',
-                                    drned_directory='drned_dir'))
+    rootmock = Mock(devices=Mock(device={DEVICE_NAME: device}),
+                    packages=Mock(package={'drned-xmnr': Mock(directory=XMNR_INSTALL)}),
+                    drned_xmnr=Mock(xmnr_directory=XMNR_DIRECTORY,
+                                    drned_directory=DRNED_DIRECTORY,
+                                    xmnr_log_file=None))
+    ncs_items = ['_ncs.stream_connect', '_ncs.dp.action_set_timeout']
     with patch('ncs.maapi.Maapi', return_value=MaapiMock()) as maapi:
         with patch('ncs.maagic.get_root', return_value=rootmock):
-            ncs_items = ['maapi', 'dp', 'stream_connect']
-            with nest_mgrs([patch('_ncs.{}'.format(ncs_item))
-                            for ncs_item in ncs_items]) as ncs_patches:
+            with nest_mgrs([patch(ncs_item) for ncs_item in ncs_items]) as ncs_patches:
                 mock_inst = XtestMock('ncs')
+                items = [it.split('.')[-1] for it in ncs_items]
                 mock_inst.data = dict(maapi=maapi,
-                                      ncs=dict(zip(ncs_items, reversed(ncs_patches))))
+                                      root=rootmock,
+                                      device=device,
+                                      ncs=dict(zip(items, reversed(ncs_patches))))
                 yield mock_inst
 
 
@@ -103,87 +117,45 @@ class FileData(object):
         pass
 
 
+class StreamData(object):
+    """Auxiliary class for mocking anything that provides streamed data."""
+    def __init__(self, data, chunk):
+        if data == []:
+            self.data = []
+        else:
+            self.data = [data[i*chunk:(i+1)*chunk]
+                         for i in range(0, (len(data)+chunk-1)//chunk)]
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.finished():
+            raise StopIteration()
+        return self.data.pop(0)
+
+    def finished(self):
+        return self.data == []
+
+
 class SystemMock(XtestMock):
-    def __init__(self, patches):
+    def __init__(self, ff_patcher, patches):
         super(SystemMock, self).__init__('system')
-        self.tree = {}
         self.patches = patches
-        for fn, fnmock in patches['os'].items():
-            fnmock.side_effect = functools.partial(self.invoke_system, fnmock, fn)
+        self.ff_patcher = ff_patcher
         socket = patches['socket']['socket']
         socket.return_value = mock.Mock(recv=Mock(side_effect=self.get_socket_data))
         self.socket_data('')
 
-    def invoke_system(self, mock, name, *args):
-        if name == 'path.exists':
-            return self.exists(*args)
-        if name == 'makedirs':
-            return self.makedirs(*args)
-        if name == 'remove':
-            return self.remove(*args)
-        if name == 'open':
-            return self.open(*args)
-        print('operation', name, 'not supported')
-        return mock
-
-    def set_open_mocks(self, openmock):
-        for omock in openmock.mocks:
-            omock.side_effect = functools.partial(self.invoke_system, omock, 'open')
-
-    def descend_tree(self, path, start=None):
-        node = self.tree if start is None else start
-        part_iter = iter(path.split('/') if type(path) is str else path)
-        for part in part_iter:
-            child = node.get(part, None)
-            if type(child) is not dict:
-                return node, [part] + list(part_iter), child
-            node = child
-        return node, None, None
-
-    def makedirs(self, path, start=None):
-        node, rest, child = self.descend_tree(path, start=start)
-        if type(child) is dict:
-            raise RuntimeError('Directory exists: {}'.format(path))
-        if child is not None:
-            raise RuntimeError('Cannot create directory: {}'.format(path))
-        for part in rest:
-            node[part] = {}
-            node = node[part]
-
-    def exists(self, path):
-        print('existing', path)
-        _, _, child = self.descend_tree(path)
-        return child is not None
-
-    def remove(self, path):
-        node, rest, child = self.descend_tree(path)
-        if type(child) is not dict:
-            raise RuntimeError('cannot remove: {}'.format(path))
-        del node[rest[0]]
-
-    def open(self, path, attr='r'):
-        node, rest, child = self.descend_tree(path)
-        if attr == 'r':
-            if isinstance(child, FileData):
-                return child
-            raise RuntimeError('cannot read: {}'.format(path))
-        if attr == 'w':
-            if isinstance(child, dict):
-                raise RuntimeError('cannot write to {}'.format(path))
-            if len(rest) > 1:
-                prest = rest[:-1]
-                self.makedirs(prest, start=node)
-                node, _, _ = self.descend_tree(prest, start=node)
-            name = rest[-1]
-            node[name] = FileData(name)
-            return node[name]
-
     def socket_data(self, data, chunk=10):
-        self.data_iter = (data[i*chunk:(i+1)*chunk] for i in range(0, (len(data)+chunk-1)//chunk))
+        self.socket_data_iter = self.chunk_data(data, chunk)
+
+    def chunk_data(self, data, chunk):
+        return (data[i*chunk:(i+1)*chunk] for i in range(0, (len(data)+chunk-1)//chunk))
 
     def get_socket_data(self, *args):
         try:
-            return next(self.data_iter)
+            return next(self.socket_data_iter)
         except StopIteration:
             return ''
 
@@ -192,16 +164,12 @@ class SystemMock(XtestMock):
 def system_mock():
     """Mock system-level functions.
 
-    Functions from os, fcntl, glob, pickle, select, shutil, socket,
+    Functions from fcntl, pickle, select, socket,
     subprocess are mocked to avoid tests touching the system state.
     """
-    calls = {'os': ['remove', 'path.exists', 'rename', 'symlink',
-                    'mkdir', 'makedirs'],
-             'fcntl': ['fcntl'],
-             'glob': ['glob'],
+    calls = {'fcntl': ['fcntl'],
              'pickle': ['load', 'dump'],
              'select': ['select'],
-             'shutil': ['copy', 'copyfile', 'copytree', 'rmtree'],
              'socket': ['socket'],
              'subprocess': ['Popen']}
 
@@ -211,28 +179,14 @@ def system_mock():
             yield dict(zip(calls[name], reversed(mgrs)))
 
     with nest_mgrs([make_patch_group(name) for name in calls]) as patchlist:
-        # os.environ needs special care
-        with mock.patch.dict('os.environ', {'NCS_DIR': 'tmp_ncs_dir'}):
-            patches = dict(zip(calls.keys(), reversed(patchlist)))
-            popen = patches['subprocess']['Popen']
-            wait_mock = mock.Mock(return_value=0)
-            popen.return_value = mock.Mock(wait=wait_mock)
-            yield SystemMock(patches)
-
-
-class OpenMock(XtestMock):
-    def __init__(self, open_mocks):
-        super(OpenMock, self).__init__('open')
-        self.mocks = open_mocks
-
-
-def open_mock(*modules):
-    @contextmanager
-    def cmgr():
-        with nest_mgrs([mock.patch('drned_xmnr.op.{}.open'.format(module))
-                        for module in modules]) as open_mocks:
-            yield OpenMock(open_mocks)
-    return cmgr
+        with ffs.Patcher() as ff_patcher:
+            # os.environ needs special care
+            with mock.patch.dict('os.environ', {'NCS_DIR': 'tmp_ncs_dir'}):
+                patches = dict(zip(calls.keys(), reversed(patchlist)))
+                popen = patches['subprocess']['Popen']
+                wait_mock = mock.Mock(return_value=0)
+                popen.return_value = mock.Mock(wait=wait_mock)
+                yield SystemMock(ff_patcher, patches)
 
 
 def init_mocks():
