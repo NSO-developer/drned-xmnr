3333e9ecc905eed83ea0cacd1d3fee5bd62f22e6
Michal Novak <micnovak@cisco.com>
2022-04-05 11:40:34 +0200

    drned-xmnr #107 - improved flake8_check script (common part in variable)
    Signed-off-by: Michal Novak <micnovak@cisco.com>

--- flake8-check.sh
+++ flake8-check.sh
@@ -1,4 +1,5 @@
 set -e
-FLAKE8_ARGS="--count --max-line-length=120 --statistics"
-flake8 ${XMNR_DIR}/python ${XMNR_DIR}/test/unit/ ${FLAKE8_ARGS} --show-source
-flake8 ${XMNR_DIR}/python ${XMNR_DIR}/test/unit/ ${FLAKE8_ARGS} --exit-zero --max-complexity=25
+FLAKE8_ARGS="--count --max-line-length=120 --statistics --exclude namespaces"
+FLAKE8_CMD="flake8 ${XMNR_DIR}/python ${XMNR_DIR}/test/unit/ ${FLAKE8_ARGS}"
+${FLAKE8_CMD} --show-source
+${FLAKE8_CMD} --exit-zero --max-complexity=25
