108fe2f5fea95e1b1acc1d2286f96813605e6308
Michal Novak <micnovak@cisco.com>
2022-04-05 15:09:33 +0200

    drned-xmnr #107 - improved flake8_check script (added drned-skeleton, removed stubs in python directory )
    Signed-off-by: Michal Novak <micnovak@cisco.com>

--- flake8-check.sh
+++ flake8-check.sh
@@ -1,5 +1,5 @@
 set -e
 FLAKE8_ARGS="--count --max-line-length=120 --statistics --exclude namespaces"
-FLAKE8_CMD="flake8 ${XMNR_DIR}/python ${XMNR_DIR}/test/unit/ ${FLAKE8_ARGS}"
+FLAKE8_CMD="flake8 ${XMNR_DIR}/python/drned_xmnr ${XMNR_DIR}/drned-skeleton ${XMNR_DIR}/test/unit/ ${FLAKE8_ARGS}"
 ${FLAKE8_CMD} --show-source
 ${FLAKE8_CMD} --exit-zero --max-complexity=25
